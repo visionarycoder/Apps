fcf45e93482b7463abbe944ff2f463d4b5d94bb4
Ivan Jones <ivanjo@vsp.com>
2024-05-10 15:01:12 -0700

    Noodling on the SideCar idea.

--- Swag.Calculator/Swag.Access.Data/Interface/IDataAccess.cs
+++ Swag.Calculator/Swag.Access.Data/Interface/IDataAccess.cs
@@ -2,12 +2,20 @@
 
 namespace Swag.Access.Data.Interface;
 
-public interface IDataAccess
+public interface IAbstractDataAccess : IDataAccess
+{
+    new ICollection<object> GetEstimates();
+    new object GetEstimate(int optimistic, int mostLikely, int pessimistic);
+    new object AddEstimate(Estimate estimate);
+    new object UpdateEstimate(Estimate estimate);
+    new object DeleteEstimate(int id);
+}
+
+public interface IDataAccess 
 {
     Task<IEnumerable<Estimate>> GetEstimates();
     Task<(Estimate Estimate, bool EntryFound)> GetEstimate(int optimistic, int mostLikely, int pessimistic);
     Task<(Estimate? estimate, bool successful)> AddEstimate(Estimate estimate);
     Task<(Estimate? estimate, bool successful)> UpdateEstimate(Estimate estimate);
     Task<bool> DeleteEstimate(int id);
-
 }
---
+++ Swag.Calculator/Swag.Access.Data/Sidecar/SidecarWrapper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Swag.Access.Data.Interface;
+
+namespace Swag.Access.Data.Sidecar;
+
+public class SidecarWrapper(ILogger<SidecarWrapper> logger, IDataAccess dataAccess)
+{
+
+
+
+
+}
